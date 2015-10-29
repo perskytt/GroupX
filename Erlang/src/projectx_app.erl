ecde0c9b60e1974683f57f875c3c68728a461d65
perskytt <perskytt@outlook.com>
2015-10-29 21:16:47 +0100

    Skeleton

---
+++ Erlang/src/projectx_app.erl
@@ -0,0 +1,16 @@
+-module(projectx_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    projectx_sup:start_link().
+
+stop(_State) ->
+    ok.
